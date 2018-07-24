298667d0e9eafe4e3f73ae1e63c06e88ad41770d
Leighzer <bwc_nothgiel@live.com>
2018-07-23 21:14:52 -0400

    Reworked solution

--- c++/p001/p001/p001.cpp
+++ c++/p001/p001/p001.cpp
@@ -3,41 +3,19 @@
 
 int main() {
 
-	int x = 3;
-	int y = 5;
+	int a = 3;
+	int b = 5;
 
-	int z = x*y;
+	int result = 0;
+	for (int i = 1; i < 1000; i++) {
+		if (i % a == 0 || i % b == 0) {
+			result += i;
+		}
 
-	int sumx = 0;
-	int sumy = 0;
-	int sumz = 0;
-
-	int totalSum = 0;
-
-	int i = 1;
-	while ((x*i) < 1000) {
-		sumx = sumx + x*i;
-		i++;
 	}
+	
 
-
-	i = 1;
-	while ((y*i) < 1000) {
-		sumy = sumy + y*i;
-		i++;
-	}
-
-
-	i = 1;
-	while ((z*i) < 1000) {
-		sumz = sumz + z*i;
-		i++;
-	}
-
-
-	totalSum = sumx + sumy - sumz;
-
-	std::cout << "The sum of the multiples of 3 or 5 below 1000 are: " << totalSum << std::endl;
+	std::cout << "The sum of the multiples of 3 or 5 below 1000 are: " << result << std::endl;
 
 	std::cout << "Press Enter to exit the program.";
 
