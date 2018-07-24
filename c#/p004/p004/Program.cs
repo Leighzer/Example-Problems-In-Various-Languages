1e9a33b2183d38d114ac5231add79b124a80fde1
Leighzer <bwc_nothgiel@live.com>
2018-07-23 21:52:12 -0400

    Added p004 solution

---
+++ c#/p004/p004/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> palindromes = new List<int>();
+            
+            for(int i = 999; i > 0; i--)
+            {
+                for(int j = 999; j > 0; j--)
+                {
+                    int product = i * j;
+                    if (isPalindrome(product))
+                    {
+                        palindromes.Add(product);
+                    }
+                }
+            }
+
+            palindromes.Sort();            
+            
+            Console.WriteLine("The largest palindrome product of two 3-digit numbers is :" + palindromes[palindromes.Count -1]);
+
+            Console.ReadLine();
+
+        }
+
+        static bool isPalindrome(int number)
+        {
+            String numberAsString = number.ToString();
+
+            char[] charArray = number.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            String backwardsNumber = new string(charArray);
+
+            return backwardsNumber.Equals(numberAsString);
+        }
+    }
+}
